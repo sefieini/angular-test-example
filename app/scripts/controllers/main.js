c27532302f68f8b10484898b2613ce7eb78841cb
sefi.eini <sonar004@gmail.com>
2015-07-06 14:10:28 +0300

    init

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,61 @@
+'use strict';
+
+angular.module('mytodoApp')
+  .controller('MainCtrl', ['$scope', 'localStorageService', 'SortService', function ($scope, localStorageService, SortService) {
+
+    var todosInStore = localStorageService.get('todos');
+    $scope.sortingSetting = {by: '', direction: ''}
+
+    $scope.todos = todosInStore || [];
+
+    $scope.$watch('todos', function () {
+      SortService.setItems($scope.todos);
+      $scope.sortAll();
+      localStorageService.add('todos', $scope.todos);
+    }, true);
+
+    // Uncomment if you are disabling persistence
+    //$scope.todos = [];
+    $scope.init = function () {
+      $scope.todo = {name: '', time: '00:00', priority: $scope.todos.length};
+    }();
+
+    $scope.addTodo = function () {
+      $scope.todos.push($scope.todo);
+      $scope.todo = {name: '', time: '00:00', priority: $scope.todos.length};
+    };
+
+    $scope.removeTodo = function (index) {
+      $scope.todos.splice(index, 1);
+    };
+
+    // $scope.sortAllBy = function (property) {
+    //   if ($scope.sortingSetting.by == property){
+    //     $scope.sortingSetting.property = ($scope.sortingSetting.property == 'asc' ? 'desc' : 'asc')
+    //   }else{
+    //     $scope.sortingSetting = {by: property, direction: 'asc'}
+    //   }
+
+    //   $scope.sortAll();
+    // };
+
+
+    $scope.removeAll = function () {
+      $scope.todos = [];
+
+    };
+
+    $scope.sortBy = function(property){
+      SortService.sortBy(property);
+    }
+
+    $scope.getSorterImage = function(property){       
+      if (property == SortService.property){
+        var imageName = (SortService.direction == 'asc' ? 'up' : 'down');
+        return '/images/' + imageName + '.jpeg' ;
+      }
+        
+      return undefined;
+    }
+
+  }]);
---
+++ app/scripts/services/sort_service.js
@@ -0,0 +1,35 @@
+// function SortService() {
+
+//   // this.direction = 'asc';
+//   // this.property = 'priority';
+//   // this.items = [];
+
+//   // this.setItems = setItems;
+//   // this.setDirection = setDirection;
+
+//   // function setItems(items) {
+//   //   this.items = items;
+//   // }
+  
+//   // function setDirection(direction) {
+//   //   this.direction = direction;
+//   // }
+  
+//   // function setProperty(property) {
+//   //   this.property = property;
+//   // }
+
+//   // function sort() {
+//   //   if (!this.property){
+//   //       return;
+//   //   }
+
+//   //   this.items = this.items.sort(function(a,b) { 
+//   //     var aMoreThanB = a[this.property] > b[this.property];
+//   //     return (this.direction == 'asc' ? aMoreThanB : !aMoreThanB) });
+//   //   }
+// }
+
+// angular.module('mytodoApp', [])
+//   .service('SortService', function(){});
+
