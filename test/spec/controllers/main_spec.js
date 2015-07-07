f8d215f421e2ffa6d0b346c6e8379b64cd86c233
sefi.eini <sonar004@gmail.com>
2015-07-07 18:11:26 +0300

    gitignore

--- test/spec/controllers/main_spec.js
+++ test/spec/controllers/main_spec.js
@@ -5,27 +5,92 @@ describe('Controller: MainCtrl', function () {
   // load the controller's module
   beforeEach(module('mytodoApp'));
 
-  var MainCtrl,
-    scope;
+  var MainCtrl, scope, SortService, todoInit = {name: '', time: '00:00', priority: 0};
+  var newTodo = function(num){
+      return angular.extend(angular.extend({}, todoInit), {name: 'name' + num})
+  };
 
   // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope, localStorageService, SortService) {
+  beforeEach(inject(function ($controller, $rootScope, localStorageService, _SortService_) {
     scope = $rootScope.$new();
+    SortService = _SortService_;
     MainCtrl = $controller('MainCtrl', {
       $scope: scope, localStorageService: localStorageService
     });
   }));
 
 
-
   it('#init', function () {
-    // Wrong :
-    //expect(scope.todo).toBe({name: '', time: '00:00', priority: 0});
 
-    // Right :
-    testUtils.expectAlike(scope.todo, {name: '', time: '00:00', priority: 0})
+      testUtils.expectAlike(scope.todo, todoInit);
     expect(scope.todos.length).toBe(0);
-      //scope.sortingSetting.. ?
+      testUtils.expectAlike(scope.sortingSetting, {by: '', direction: ''})
+  });
+
+  it('#addTodo', function () {
+      var uniqTodo = {name: 'uniq name', time: '12:34', priority: 1234};
+      scope.todo = uniqTodo;
+
+      expect(scope.todos.length).toBe(0);
+      scope.addTodo();
+
+      expect(scope.todos.length).toBe(1);
+      testUtils.expectAlike(scope.todos[0], uniqTodo);
+      var todoAfterAdding = angular.copy(todoInit);
+      todoAfterAdding.priority = 1;
+      testUtils.expectAlike(scope.todo, todoAfterAdding);
+
+      scope.addTodo();
+      expect(scope.todos.length).toBe(2);
+      todoAfterAdding.priority = 2;
+      testUtils.expectAlike(scope.todo, todoAfterAdding);
+  });
+
+  it('#removeTodo', function () {
+
+      scope.todos = [newTodo(1), newTodo(2), newTodo(3)];
+
+      expect(scope.todos.length).toBe(3);
+      expect(scope.todos[1].name).toBe('name2');
+      scope.removeTodo(1);
+      expect(scope.todos[1].name).not.toBe('name2');
+
+      expect(scope.todos[0].name).toBe('name1');
+      scope.removeTodo(0);
+      expect(scope.todos[0].name).not.toBe('name1');
+  });
+
+  it('#removeAll', function () {
+      scope.todos = [newTodo(1), newTodo(2), newTodo(3)];
+
+      expect(scope.todos.length).toBe(3);
+      scope.removeAll();
+      expect(scope.todos.length).toBe(0);
+  });
+
+  it('#sortBy', function () {
+      spyOn(SortService, 'sortBy')
+      scope.sortBy('data');
+      expect(SortService.sortBy).toHaveBeenCalledWith('data');
+  });
+
+  describe('#getSorterImage', function () {
+      beforeEach(function(){
+          SortService.property = 'property1';
+          SortService.direction = 'asc';
+      })
+
+      it('should turn away direction if same direction', function(){
+          expect(scope.getSorterImage('property1')).toBe('/images/up.jpeg');
+          SortService.direction = 'desc';
+          expect(scope.getSorterImage('property1')).toBe('/images/down.jpeg');
+      })
+
+      it('should turn return undefined for a different property regardless direction', function(){
+          expect(scope.getSorterImage('property2')).toBe(undefined);
+          SortService.direction = 'desc';
+          expect(scope.getSorterImage('property2')).toBe(undefined);
+      })
   });
 
 });
