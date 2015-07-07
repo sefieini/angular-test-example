ff75fe22c80ca0a257c62d2c386b046c7c5d6ba1
sefi.eini <sonar004@gmail.com>
2015-07-07 19:01:33 +0300

    sort_service into right path

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,12 +1,6 @@
 
 'use strict';
 
-
-function SortService() {
-
-}
-
-
 var app = angular.module('mytodoApp', [
   'ngCookies',
   'ngResource',
@@ -27,42 +21,4 @@ var app = angular.module('mytodoApp', [
       });
   }]);
 
-  app.service('SortService', [function(){
 
-
-  this.direction = 'asc';
-  this.property = 'priority';
-  this.items = [];
-
-  this.setItems = setItems;
-  this.sort = sort;
-  this.sortBy = sortBy;
-
-  function setItems(items) {
-    this.items = items;
-    //this.sort(); // AUTO-Sorting
-  }
-  
-  function sortBy(property){
-    if (this.property == property){
-      this.direction = (this.direction == 'asc' ? 'desc' : 'asc');
-    }else{
-      this.property = property;
-      this.direction = 'asc';
-    }
-    this.sort();
-
-  }
-
-  function sort() {
-    var property = this.property;
-    var direction = this.direction;
-    if (!property){
-        return;
-    }
-
-    this.items = this.items.sort(function(a,b) { 
-      var aMoreThanB = a[property] > b[property];
-      return (direction == 'asc' ? aMoreThanB : !aMoreThanB) });
-    }
-  }]);
