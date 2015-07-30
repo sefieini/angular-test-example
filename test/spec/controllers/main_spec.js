f368ae328f7c500d0aab7676986820158ba959a5
sefi.eini <sonar004@gmail.com>
2015-07-30 12:23:25 +0300

    adding some test with group

--- test/spec/controllers/main_spec.js
+++ test/spec/controllers/main_spec.js
@@ -16,6 +16,10 @@ describe('Controller: MainCtrl', function () {
     });
   }));
 
+    var expectEmptyTodo = function(priority){
+        priority = priority || 0;
+        expect(scope.todo).toEqual({name: '', time: '00:00', priority: priority});
+    }
 
 
   it('#init', function () {
@@ -23,9 +27,26 @@ describe('Controller: MainCtrl', function () {
     //expect(scope.todo).toBe({name: '', time: '00:00', priority: 0});
 
     // Right :
-    testUtils.expectAlike(scope.todo, {name: '', time: '00:00', priority: 0})
+    expectEmptyTodo();
     expect(scope.todos.length).toBe(0);
       //scope.sortingSetting.. ?
   });
+
+    it('#addTodo', function(){
+        var todo = {name: 'test-name', time: '00:34', priority: 1234};
+        scope.todo = todo;
+        var currLength = scope.todos.length;
+        scope.addTodo();
+        expect(scope.todos.length).toBe(currLength + 1);
+        expect(scope.todos[currLength]).toEqual(todo);
+        expectEmptyTodo(1);
+    })
+
+    it('#removeTodo', function(){
+        scope.todos = [{name: 'test-name', time: '00:34', priority: 1234}];
+        spyOn(scope.todos, 'splice');
+        scope.removeTodo(1324);
+        expect(scope.todos.splice).toHaveBeenCalledWith(1324, 1);
+    })
 
 });
